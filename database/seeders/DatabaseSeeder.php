970eca8e769d738a1f9c4026dfda206104edbf9a
anggaferdani <eanggawahyu@gmail.com>
2024-09-09 17:30:22 +0700

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,7 @@ class DatabaseSeeder extends Seeder
                 'email' => 'admin@smart-absensi.com',
                 'password' => bcrypt('admin123*'),
                 'phone' => 123456789,
+                'profile_picture' => 'default.png',
                 'role' => 1,
             ],
         ];
