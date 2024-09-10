f11342bea3333b550a756f00ae8d334ec15d31f1
anggaferdani <eanggawahyu@gmail.com>
2024-09-10 20:51:05 +0700

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,18 +18,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // $users = [
-        //     [
-        //         'name' => 'Admin',
-        //         'email' => 'admin@smart-absensi.com',
-        //         'password' => bcrypt('admin123*'),
-        //         'phone' => 123456789,
-        //         'profile_picture' => 'default.png',
-        //         'role' => 1,
-        //     ],
-        // ];
+        $users = [
+            [
+                'name' => 'Admin',
+                'email' => 'admin@smart-absensi.com',
+                'password' => bcrypt('admin123*'),
+                'phone' => 123456789,
+                'profile_picture' => 'default.png',
+                'role' => 1,
+            ],
+        ];
 
-        // User::insert($users);
+        User::insert($users);
 
         $contactPersons = [
             [
