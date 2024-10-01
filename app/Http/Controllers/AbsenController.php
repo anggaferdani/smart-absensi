e3d28ca7def3fd77d28e1d3e9a39fed13a22e1ee
anggaferdani <eanggawahyu@gmail.com>
2024-10-01 11:19:09 +0700

    print laporan

--- app/Http/Controllers/AbsenController.php
+++ app/Http/Controllers/AbsenController.php
@@ -164,25 +164,27 @@ class AbsenController extends Controller
             $users = User::with('absens')->get();
             
             $bulan = $request->has('bulan') && !empty($request->input('bulan'))
-                ? $request->input('bulan')
-                : ($request->has('tanggal') && !empty($request->input('tanggal'))
-                    ? Carbon::parse($request->input('tanggal'))->format('Y-m')
-                    : Carbon::now()->format('Y-m')
-                );
+                ? Carbon::parse($request->input('bulan'))->format('Y-m')
+                : Carbon::now()->format('Y-m');
 
             $userLateness = $users->mapWithKeys(function($user) use ($bulan) {
                 $lateCount = $user->absens->filter(function($absen) use ($bulan) {
-                    return $absen->status == 3 && $absen->token->status == 1 && Carbon::parse($absen->tanggal)->format('Y-m') == $bulan;
+                    return $absen->status == 3 
+                        && $absen->token->status == 1 
+                        && Carbon::parse($absen->tanggal)->format('Y-m') == $bulan;
                 })->count();
                 return [$user->id => $lateCount];
             });
 
             $userOvertime = $users->mapWithKeys(function($user) use ($bulan) {
                 $overtime = $user->absens->filter(function($absen) use ($bulan) {
-                    return $absen->status == 3 && $absen->token->status == 2 && Carbon::parse($absen->tanggal)->format('Y-m') == $bulan;
+                    return $absen->status == 3 
+                        && $absen->token->status == 2 
+                        && Carbon::parse($absen->tanggal)->format('Y-m') == $bulan;
                 })->count();
                 return [$user->id => $overtime];
             });
+
     
             $months = $absens->groupBy(function($date) {
                 return Carbon::parse($date->tanggal)->format('F Y');
